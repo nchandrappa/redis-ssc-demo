6e32714ece799e68b9a002506b7ce908f71fdf23
liwang-pivotal <liwang@pivotal.io>
2018-09-13 12:55:53 -0700

    extract hostname properties

--- src/main/java/demo/Config.java
+++ src/main/java/demo/Config.java
@@ -1,16 +1,22 @@
 package demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class Config {
+	
+	@Autowired
+	private Environment env;
+	
 	@Bean
 	JedisConnectionFactory jedisConnectionFactory() {
 		JedisConnectionFactory jedisConFactory = new JedisConnectionFactory();
-		jedisConFactory.setHostName("127.0.0.1");
+		jedisConFactory.setHostName(env.getProperty("app.host"));
 		jedisConFactory.setPort(6379);
 		return jedisConFactory;
 	}
